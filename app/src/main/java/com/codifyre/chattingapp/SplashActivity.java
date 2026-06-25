c40575efd9b7c3bd3f5b59315e415e67b1c22430
UrvashiAmbarate <ambarateurvashi1227@gmail.com>
2026-06-25 22:52:16 +0530

    new changes

--- app/src/main/java/com/codifyre/chattingapp/SplashActivity.java
+++ app/src/main/java/com/codifyre/chattingapp/SplashActivity.java
@@ -1,24 +1,43 @@
 package com.codifyre.chattingapp;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.codifyre.chattingapp.LoginActivity;
+import com.codifyre.chattingapp.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    //Classname objectname;
+    LinearLayout llMain;
+    ImageView ivMainLogo;
+    TextView tvMainTitle, tvMainSlogan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+
+        Handler handler = new Handler();
+        handler .postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Classname objectname = new Cinstructorname(FirstActivity.this, SecondActivity.class);
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
     }
 }
